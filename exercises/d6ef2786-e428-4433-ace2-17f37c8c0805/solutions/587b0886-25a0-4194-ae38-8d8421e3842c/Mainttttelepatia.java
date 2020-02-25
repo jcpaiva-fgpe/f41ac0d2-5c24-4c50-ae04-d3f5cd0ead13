c4e4745ee990a0d3d11efa08353ead7e9026a897
José Paiva <test@test.com>
2020-02-25 16:45:33 +0000

    updated file exercises/d6ef2786-e428-4433-ace2-17f37c8c0805/solutions/587b0886-25a0-4194-ae38-8d8421e3842c/Mainttttelepatia.java

---
+++ exercises/d6ef2786-e428-4433-ace2-17f37c8c0805/solutions/587b0886-25a0-4194-ae38-8d8421e3842c/Mainttttelepatia.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MainTelepatia {
+	
+	
+	public static void main(String[] args) {
+		if (ant==1) 
+			pontos++;
+		else 
+			pontos = pontos + ant*3;
+		System.out.println(acertos);
+		System.out.println(pontos);
+		first.close();
+		second.close();
+
+
+        blah
+
+        
+		in.close();
+		
+	}
+}
